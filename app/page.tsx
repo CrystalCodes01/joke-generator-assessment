15c4f3c98b8258ac97c57df3df06059f09d08bec
cschurko <cschurko@banyanhill.com>
2024-02-09 12:17:11 -0500

    Material provider, Basic fetch to see the data in console, Responsive grid base

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
+"use client";
+
+import React, { useEffect } from "react";
+import { Container, Grid } from "@mui/material";
+
+const Home: React.FC = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          "https://mwks-joke-service.azurewebsites.net/api/joke/random"
+        );
+        if (!res.ok) {
+          throw new Error(`error: ${res.status}`);
+        }
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <main>
-     <h1>Home</h1>
+      <Container maxWidth="lg">
+        <Grid container>
+          <h1>Home</h1>
+        </Grid>
+      </Container>
     </main>
   );
 }
+
+export default Home;
+
