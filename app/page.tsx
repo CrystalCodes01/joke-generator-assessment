1ced3200e7a8515df36aa9dd319fe18e1e36dc0f
cschurko <cschurko@banyanhill.com>
2024-02-09 13:12:19 -0500

    show joke state, error, loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import React, { useState } from "react";
 import { Container, Grid, Box, Typography } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import useFetch from "./hooks/useFetch";
 import Header from "./components/Header";
+import PrimaryButton from "./components/PrimaryButton";
 
 // Get the green & Blue from mockup to use for styling
 const theme = createTheme({
@@ -27,6 +29,9 @@ const Home: React.FC = () => {
     punchLine: string;
   }
 
+  const [showJoke, setShowJoke] = useState<boolean>(false);
+  const toggleShow = () => setShowJoke((prev) => !prev);
+
   // useFetch hook with Joke type
   const {
     data: joke,
@@ -48,16 +53,58 @@ const Home: React.FC = () => {
               linkUrl="https://mwks-joke-service.azurewebsites.net/swagger/index.html"
               linkText="View API Docs"
             />
+            {/* need a border here */}
             <Grid item xs={12} mt={6}>
-              <h1>{joke?.joke}</h1>
-              <h2>{joke?.punchLine}</h2>
-              <Box textAlign="center">
-                <Typography color="error" fontWeight="bold">
-                  THERE WAS AN ERROR LOADING YOUR JOKE.
-                </Typography>
-              </Box>
-              <Box textAlign="center">
-                <Typography fontWeight="bold">LOADING YOUR JOKE...</Typography>
+              {error && (
+                <Box textAlign="center">
+                  <Typography color="error" fontWeight="bold">
+                    THERE WAS AN ERROR LOADING YOUR JOKE.
+                  </Typography>
+                </Box>
+              )}
+              {/* if loading or data request in progress show loading message */}
+              {!joke || isLoading ? (
+                <Box textAlign="center">
+                  <Typography fontWeight="bold">
+                    LOADING YOUR JOKE...
+                  </Typography>
+                </Box>
+              ) : (
+                <>
+                  <Box
+                    p={2}
+                    display="flex"
+                    flexDirection="column"
+                    className="quote-wrap-start"
+                  >
+                    {joke && <Typography fontSize={26}>{joke.joke}</Typography>}
+                  </Box>
+                  <Box
+                    p={2}
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    textAlign="center"
+                    mt={2}
+                  >
+                    <PrimaryButton
+                      variant="contained"
+                      text={!showJoke ? "Show Punchline" : "Hide Punchline"}
+                      onClick={toggleShow}
+                      color="secondary"
+                    />
+                  </Box>
+                </>
+              )}
+              <Box
+                p={2}
+                display="flex"
+                flexDirection="column"
+                alignItems="flex-end"
+                justifyContent="flex-end"
+              >
+                <Typography fontSize={26}>{joke?.punchLine}</Typography>
               </Box>
             </Grid>
           </Grid>
